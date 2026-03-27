499f2a5ea1689651c996144412b18e3ab8b05b61
kartoshkas13 <126.24boiko.o@nltu.lviv.ua>
2026-03-27 08:33:20 +0200

    Add files via upload

---
+++ lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxRows = new System.Windows.Forms.TextBox();
+            this.textBoxCols = new System.Windows.Forms.TextBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 227);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(300, 213);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(299, 198);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalc.TabIndex = 1;
+            this.buttonCalc.Text = "Обчислити";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxRows
+            // 
+            this.textBoxRows.Location = new System.Drawing.Point(231, 87);
+            this.textBoxRows.Name = "textBoxRows";
+            this.textBoxRows.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRows.TabIndex = 5;
+            // 
+            // textBoxCols
+            // 
+            this.textBoxCols.Location = new System.Drawing.Point(342, 87);
+            this.textBoxCols.Name = "textBoxCols";
+            this.textBoxCols.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCols.TabIndex = 6;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(92, 135);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(220, 23);
+            this.buttonCreate.TabIndex = 7;
+            this.buttonCreate.Text = "Створити матрицю";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Location = new System.Drawing.Point(354, 135);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(220, 23);
+            this.buttonRandom.TabIndex = 8;
+            this.buttonRandom.Text = "Заповнити рандомом";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(377, 250);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(292, 150);
+            this.textBoxResult.TabIndex = 9;
+            this.textBoxResult.Text = "Результат";
+            this.textBoxResult.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Рядок";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Стовпець";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.textBoxCols);
+            this.Controls.Add(this.textBoxRows);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxRows;
+        private System.Windows.Forms.TextBox textBoxCols;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonRandom;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab2/lab2/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 3;
+            dataGridView1.ColumnCount = 3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = dataGridView1.RowCount;
+            int m = dataGridView1.ColumnCount;
+
+            int[,] matrix = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (dataGridView1[j, i].Value == null)
+                    {
+                        MessageBox.Show("Заповніть всі клітинки матриці");
+                        return;
+                    }
+
+                    matrix[i, j] = Convert.ToInt32(dataGridView1[j, i].Value);
+                }
+            }
+
+            textBoxResult.Text = "";
+
+            // сума рядків з від'ємними елементами
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < m; j++)
+                {
+                    sum += matrix[i, j]; // всі елементи
+                }
+
+                textBoxResult.Text += "Рядок " + i + " сума = " + sum + Environment.NewLine;
+            }
+
+            textBoxResult.Text += Environment.NewLine + "Сідлові точки:" + Environment.NewLine;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    int element = matrix[i, j];
+
+                    bool isMinInRow = true;
+                    for (int col = 0; col < m; col++)
+                    {
+                        if (matrix[i, col] < element)
+                        {
+                            isMinInRow = false;
+                            break;
+                        }
+                    }
+
+                    bool isMaxInCol = true;
+                    for (int row = 0; row < n; row++)
+                    {
+                        if (matrix[row, j] > element)
+                        {
+                            isMaxInCol = false;
+                            break;
+                        }
+                    }
+
+                    if (isMinInRow && isMaxInCol)
+                    {
+                        textBoxResult.Text += $"i={i} j={j} value={element}\r\n";
+                    }
+                }
+            }
+
+            // сума додатних елементів над головною діагоналлю
+            if (n != m)
+            {
+                textBoxResult.Text += "\r\nМатриця не квадратна — неможливо знайти суму додатних елементів над головною діагоналлю";
+            }
+            else
+            {
+                int sumPositive = 0;
+
+                for (int a = 0; a < n; a++)
+                {
+                    for (int b = 0; b < m; b++)
+                    {
+                        if (a < b)
+                        {
+                            sumPositive += matrix[a, b];
+                        }
+                    }
+                }
+
+                textBoxResult.Text += "\r\nСума додатних елементів над головною діагоналлю = " + sumPositive;
+            }
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int rows = Convert.ToInt32(textBoxRows.Text);
+                int cols = Convert.ToInt32(textBoxCols.Text);
+
+                if (rows <= 0 || cols <= 0)
+                {
+                    MessageBox.Show("Кількість має бути більше 0");
+                    return;
+                }
+
+                dataGridView1.RowCount = rows;
+                dataGridView1.ColumnCount = cols;
+            }
+            catch
+            {
+                MessageBox.Show("Введіть числа!");
+            }
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1[j, i].Value = rand.Next(-10, 10);
+                }
+            }
+        }
+
+        private void textBoxResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
